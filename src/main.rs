ee11587998185e794b6856caf9a1f1ca2a7d2379
SLMT <sam123456777@gmail.com>
2021-08-22 21:50:42 +0800

    Add Clap and the first command

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Yu-Shan Lin (sam123456777@gmail.com)")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Command {
+    #[clap(subcommand)]
+    subcmd: SubCommand
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Create(CreateCommand)
+}
+
+#[derive(Clap)]
+struct CreateCommand {
+    fund: String
+}
+
+fn main() {
+    // Goal for v1.0
+    // Inputs: initial assets, transaction records
+    // Outputs: the timeline of price per share
+    // Others:
+    // - Auto save the data
+    // - Won't save duplicate transactions
+
+    // Command: create [FUND]
+    // Command: add-history [FUND]
+    // Command: generate-timeline [FUND]
+
+    // Parse the command
+    let opts: Command = Command::parse();
+    match opts.subcmd {
+        SubCommand::Create(cmd) => {
+            println!("Fund name: {}", cmd.fund);
+        }
+    }
+}
