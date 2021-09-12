fbdc0c577c30e28dcd691bb60e4bdc451a3dcea5
SLMT <sam123456777@gmail.com>
2021-09-12 21:27:55 +0800

    Add the request to get name in 'create' cmd

--- src/command/create.rs
+++ src/command/create.rs
@@ -1,16 +1,17 @@
 
+use std::io::Write;
+
 use colored::*;
 
-use crate::error::AnalyzerResult;
+use crate::error::{AnalyzerError, AnalyzerResult};
 
 pub fn create(fund_id: &str) -> AnalyzerResult<()> {
   // TODO: check duplicate fund
   
-  display_welcome_message(&fund_id);
+  display_welcome_message(fund_id);
+  check_id_format(fund_id)?;
+  let fund_name = request_fund_name()?;
 
-  // TODO: check fund ID format (1-9, a-z, '_')
-
-  // TODO: get fund name
   // TODO: get intial portfolio
   // TODO: show the result and asking if it is needed to change
   // TODO: show ending message (where the data are saved)
@@ -20,4 +21,38 @@ pub fn create(fund_id: &str) -> AnalyzerResult<()> {
 
 fn display_welcome_message(fund_id: &str) {
   println!("Creating a new fund: {}", fund_id.yellow());
+}
+
+fn check_id_format(fund_id: &str) -> AnalyzerResult<()> {
+  for ch in fund_id.chars() {
+    if !ch.is_ascii_alphanumeric() && ch != '_' {
+      return Err(AnalyzerError::FundIdFormatError);
+    }
+  }
+
+  Ok(())
+}
+
+fn request_fund_name() -> AnalyzerResult<String> {
+  // Promot
+  print!("Please enter the name of the fund: ");
+  std::io::stdout().flush()?;
+
+  // Inputs
+  let mut input = String::new();
+  std::io::stdin().read_line(&mut input)?;
+  let input = input.trim();
+
+  // Show the name
+  println!("The name is `{}`", input.yellow());
+
+  Ok(input.to_owned())
+}
+
+fn request_initial_portfolio() -> AnalyzerResult<()> {
+  todo!();
+}
+
+fn request_cash_amount() -> AnalyzerResult<()> {
+  todo!();
 }
--- src/error.rs
+++ src/error.rs
@@ -5,5 +5,11 @@ pub type AnalyzerResult<T> = Result<T, AnalyzerError>;
 
 #[derive(Error, Debug)]
 pub enum AnalyzerError {
-
+  #[error("the fund ID must be in 0-9, a-z, A-Z, or '_'.")]
+  FundIdFormatError,
+  #[error("io error")]
+  Io { 
+    #[from]
+    source: std::io::Error
+  }
 }
